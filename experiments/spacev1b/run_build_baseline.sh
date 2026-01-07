fcb9631322d656dec97cc7a2145301fb8dfd267d
Dongfang Zhao <dzhao@uw.edu>
2026-01-07 23:16:54 +0000

    update build script

--- experiments/spacev1b/run_build_baseline.sh
+++ experiments/spacev1b/run_build_baseline.sh
@@ -2,11 +2,12 @@
 # SpaceV-1B Index Build Script (Baseline)
 
 DATA_PATH="/home/cc/hpdic/spacev1b_data/spacev1b_base.bin"
-INDEX_PREFIX="/home/cc/hpdic/spacev1b_data/spacev1b_index"
+INDEX_DIR="/home/cc/hpdic/spacev1b_data/indices_baseline" #TODO Update this path as needed
+INDEX_PREFIX="${INDEX_DIR}/spacev1b"
 BUILD_RAM_LIMIT=200  # 利用你的 251GB 内存，设为 200 可以极速构建
 
 # 确保 build 目录存在
-mkdir -p $(dirname "${INDEX_PREFIX}")
+mkdir -p "${INDEX_DIR}"
 
 echo "Starting SpaceV-1B Index Build..."
 # 注意 data_type 是 int8
